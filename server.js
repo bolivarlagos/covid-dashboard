7ae5c016e3d8e2052b1c3c210fa1779988361f42
bolivarlagos <bolivarlagos@gmail.com>
2022-01-11 20:01:22 -0300

    add ejs template engine

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express")
+const axios = require("axios")
 const app = express()
+const { PORT, URL } = require("./utils")
 
-app.listen(3000)
+app.listen(PORT || 3000)
 
 app.set("view engine", "ejs")
 
 app.use(express.static(__dirname + "/public"))
 
-app.get("/", (req, res) => {
-    res.render("index")
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(`${URL}/api/countries/`)
+        const countries = response.data  
+        res.render("index", { countries })      
+    } catch (error) {  
+        res.render("error")      
+    }               
 })
---
+++ utils.js
@@ -0,0 +1,5 @@
+require('dotenv').config()
+
+const { PORT, URL } = process.env 
+
+module.exports = { PORT, URL }
