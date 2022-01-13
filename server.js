7616c9c3bb138459342616a95644de5da6ef6173
bolivarlagos <bolivarlagos@gmail.com>
2022-01-13 11:51:57 -0300

    add partials

--- server.js
+++ server.js
@@ -11,19 +11,33 @@ app.use(express.static(__dirname + "/public"))
 
 app.get("/", async (req, res) => {
     try {
-        const response = await axios.get(URL)
+        const response = await axios.get(`${URL}/api/world`)
         const world = response.data
         const worldString = JSON.stringify(world)
-        res.render("index", { item: worldString })      
+        res.render("world", { title: "world", item: worldString })      
     } catch (error) {  
         res.render("error")      
     }               
 })
 
-app.get("/continents", (req, res) => {
-    res.render("continents")
+app.get("/continents", async (req, res) => {
+    try {
+        const response = await axios.get(`${URL}/api/continents`)
+        const continents = response.data 
+        const continentsString = JSON.stringify(continents)
+        res.render("continents", { title: "continents", item: continentsString })
+    } catch (error) {
+        res.render("error")        
+    }
 })
 
-app.get("/countries", (req, res) => {
-    res.render("countries")
+app.get("/countries", async (req, res) => {
+    try {
+        const response = await axios.get(`${URL}/api/countries`)
+        const countries = response.data 
+        const countriesString = JSON.stringify(countries)
+        res.render("countries", { title: "countries", item: countriesString })
+    } catch (error) {
+        res.render("error")        
+    }
 })
